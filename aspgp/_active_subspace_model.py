The derivative of the stiefel prior was wrong.

aspgp/_active_subspace_model.py
@@ -14,7 +14,7 @@ Date:
 """
 
 
-from . import opt_stiefel_gbb
+from . import optimize_stiefel_seq
 from . import ActiveSubspaceKernel
 from . import ParallelizedGPRegression
 import math
@@ -66,19 +66,8 @@ class ActiveSubspaceGPRegression(ParallelizedGPRegression):
         """
         Optimize the object with respect to W.
         """
-        ## Initialize W using least squares
-        #XX = np.hstack([np.ones((self.X.shape[0], 1)), self.X])
-        #a, r = np.linalg.lstsq(XX, self.Y.flatten())[:2]
-        #a = a[:, None]
-        #W = (a[1:, :] / np.linalg.norm(a[1:, :]))
-        #res = opt_stiefel_gbb(_W_obj_fun, W, args=(self,), 
-        #                      **stiefel_options)
-        res = opt_stiefel_gbb(_W_obj_fun, self.kern.W, args=(self,), 
-                              **stiefel_options)
-#                disp=False,
-#                              xtol=1e-6, ftol=1e-6, gtol=1e-6,
-#                              mxitr=1000)
-#        #                       **stiefel_options)
+        res = optimize_stiefel_seq(_W_obj_fun, self.kern.W, args=(self,),
+                                   **stiefel_options)
         self.kern.W = res.X
 
     def _optimize_other(self, **kwargs):
@@ -111,9 +100,9 @@ class ActiveSubspaceGPRegression(ParallelizedGPRegression):
         while nit <= max_it:
             self.x.append(nit) #Store iteration number history
             try:
-                self.l.append(np.float(self.kern.rbf.lengthscale.view()))
+                self.l.append(np.float(self.kern.inner_kernel.lengthscale.view()))
             except:
-                self.l.append(np.asarray(self.kern.rbf.lengthscale.view()))
+                self.l.append(np.asarray(self.kern.inner_kernel.lengthscale.view()))
             nit += 1
             old_log_like = -self.objective_function()
             self.y.append(old_log_like) #Store log likelihood history 

aspgp/_stiefel.py
@@ -62,22 +62,33 @@ def Y_func(tau, X, A):
     return np.dot(Q, X)
 
 
-#def ls_func(tau, func, func_args, X, A):
-#    return func(Y_func(tau, X, A), *func_args)[0]
+def func_obj_seq(X, dmax, func, *args):
+    """
+    An objective function used by ``optimize_stiefel_seq``.
+    """
+    n = X.shape[0]
+    d = X.shape[1]
+    F, G = func(X, *args)
+    return F, G[:, :d]
 
 
-def optimize_stiefel_seq(func, X0, args=(), tau_max=0.1, max_it=100, tol=1e-3,
-                         disp=False):
+def func_obj_seq_w_zeros(X, dmax, func, *args):
+    """
+    An objective function used by ``optimize_stiefel_seq``.
+    """
+    n = X.shape[0]
+    d = X.shape[1]
+    F, G = func(np.hstack([X, np.zeros((n, dmax - d))]), *args)
+    return F, G[:, :d]
+
+
+def optimize_stiefel_seq(func, X0, args=(), tau_max=0.1, max_it=100, tol=1e-6,
+                         disp=False, obj_func=func_obj_seq_w_zeros):
     """
     Optimize stiefel sequentially.
     """
     dmax = X0.shape[1]
-    def func_tmp(X, dmax, *args):
-        n = X.shape[0]
-        d = X.shape[1]
-        F, G = func(X, *args)
-        return F, G[:, :d]
-    res = optimize_stiefel(func_tmp, X0[:, :1], args=(dmax,) + args, tau_max=tau_max,
+    res = optimize_stiefel(obj_func, X0[:, :1], args=(dmax,func) + args, tau_max=tau_max,
                            max_it=max_it, tol=tol, disp=disp)
     for d in xrange(2, dmax + 1):
         Xd0 = res.X
@@ -86,8 +97,8 @@ def optimize_stiefel_seq(func, X0, args=(), tau_max=0.1, max_it=100, tol=1e-3,
         A = np.hstack([Xd0, xr])
         Q, R = np.linalg.qr(A)
         xr = Q[:, -1:]
-        X0 = np.hstack([Xd0, xr])
-        res = optimize_stiefel(func_tmp, X0, args=(dmax,) + args, tau_max=tau_max,
+        Xd0 = np.hstack([Xd0, xr])
+        res = optimize_stiefel(obj_func, Xd0, args=(dmax,func) + args, tau_max=tau_max,
                                max_it=max_it, tol=tol, disp=disp)
     return res
 
@@ -143,7 +154,7 @@ def optimize_stiefel(func, X0, args=(), tau_max=1., max_it=100, tol=1e-3,
             tau_init = np.linspace(0, tau_max, 3)[:, None]
             tau_d = np.linspace(0, tau_max, 50)[:, None]
             tau_all, F_all = pybgo.minimize(ls_func, tau_init, tau_d, fixed_noise=1e-16,
-                    add_at_least=1, tol=1e-3, scale=True,
+                    add_at_least=1, tol=1e-3, scale=False, callback=pybgo.plot_summary,
                     train_every=1)[:2]
             nfev += tau_all.shape[0]
             idx = np.argmin(F_all)

aspgp/_stiefel_prior.py
@@ -44,7 +44,7 @@ class StiefelPrior(GPy.priors.Prior):
         return 0.5 * self.alpha * W ** 2
 
     def lnpdf_grad(self, W):
-        return -self.alpha * W
+        return self.alpha * W
 
     def rvs(self, n):
         W = np.ndarray((n, self.input_dim))

unittests/test_steifel.py
@@ -11,13 +11,13 @@ def f(X, A):
     G = np.einsum('jk,ki->ji', A, X)
     return F, G
 
-dim = 100
+dim = 10
 sdim = 2
 A = scipy.linalg.toeplitz([2, -1] + [0] * (dim-2))
 w, v = scipy.linalg.eigh(A)
 
 X0 = np.random.randn(dim, sdim)
-X0 = scipy.linalg.orth(X0)
+X0 = X0 / np.linalg.norm(X0, axis=0)
 
 res = optimize_stiefel_seq(f, X0, args=(A,), tau_max=1., max_it=10000, tol=1e-20,disp=True)
 X = res.X